241821c6f1a5ae44ae97203146f30fee02b18152
Corentin PREVOST <prevost.corentin@yahoo.com>
2020-10-05 10:59:33 +0200

    Base code for issue #1

--- DailyShufflerToulouse/Program.cs
+++ DailyShufflerToulouse/Program.cs
@@ -4,9 +4,28 @@ namespace DailyShufflerToulouse
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(String[] args)
         {
-            Console.WriteLine("Hello World!");
+            String[] toulouseStudents =
+                { "ANGLADE Emilie", "BAGES Sébastien", "BRULTET Sophie", "BUI Coline",
+                  "DELSOL Emilie", "DESNOUES Fabien", "DUMAS Maylis", "DURCY Colas", "ESPASA Coralie",
+                  "GILLIOTTE Fabien", "IZARD Julien", "LY François", "MESTE Hervé",
+                  "SAID OMAR Gabriel", "ZAPICO Adrien" };
+            toulouseStudents = ShuffleStudents(toulouseStudents);
+            foreach (String student in toulouseStudents)
+            {
+                Console.WriteLine(student);
+            }
+        }
+
+        static String[] ShuffleStudents(String[] students)
+        {
+            // Pour i allant de 0 à students.taille - 1:
+                // j = GenererEntierAleatoireEntre(0, i)
+                // k = students[i]
+                // students[i] = students[j]
+                // students[j] = k
+            return students;
         }
     }
 }
